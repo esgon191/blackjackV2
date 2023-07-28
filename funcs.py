69b7cdfd10830b1a94e3618fe287619412c235e0
esgon <timurkaftaranov@MacBook-Pro-Timur.local>
2023-07-28 04:33:37 +0300

    deck creation and preporation

---
+++ funcs.py
@@ -0,0 +1,17 @@
+import random
+
+def make_deck() #создает колоду
+    deck = []
+    for i in ['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K']:
+        for j in ['H', 'T', 'C', 'S']:
+            deck.append([i, j])
+
+    return deck
+
+def prepare_deck(old): #мешает колоду
+    new = []
+    while len(old) > 0:
+        new.append(old.pop(random.randinti()))
+    return new
+
+
