ba08b6b6c06e09d12c16a702d3085ac553069b6c
esgon <timurkaftaranov@MacBook-Pro-Timur.local>
2023-07-28 07:06:01 +0300

    start

--- classes.py
+++ classes.py
@@ -1,34 +1,5 @@
-class Character:
-    def __init__(self, money, cards):
-        self.money = money
-        self.cards = cards
-        self.points = 0
-        slef.count_points(cards)
-
-    def count_points(self, card):
-        if card[0] == 'A':
-            if self.points <= 21:
-                self.points += 11
-
-            else:
-                self.points += 1
-
-        elif card[0] in ['J', 'Q', 'K']:
-            self.points += 10
-
-        else:
-            self.points += int(card[0])
-
-    def receive_card(self, card):
-        self.cards.append(card)
-        self.countpoints(card)
-
-class Dealer(Character):
-    def turn(self):
-        if self.points <= 16:
-            self.receive_card()
+class Card:
+    def __init__(self, iden):
+        self.iden = iden
 
 
-class Player(Character):
-    def bet(self, bet):
-        self.bet = bet
